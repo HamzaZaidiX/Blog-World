fdd99173684106580320bb721bb49c9cb369ddd1
SHJZ <syedhamzajaffarzaidi@gmail.com>
2022-11-21 03:27:39 +0500

    Add files via upload

---
+++ src/Note.tsx
@@ -0,0 +1,49 @@
+import { Badge, Button, Col, Row, Stack } from "react-bootstrap"
+import ReactMarkdown from "react-markdown"
+import { Link, useNavigate } from "react-router-dom"
+import { useNote } from "./NoteLayout"
+
+export type NoteProps = {
+    onDelete: (id: string) => void
+}
+
+export function Note({ onDelete }: NoteProps) {
+    const note = useNote()
+    const navigate = useNavigate()
+
+    return (
+        <>
+            <Row className="align-items-center mb-4">
+                <Col>
+                    <h1 className="text-center">{note.title}</h1>
+                    {note.tags.length > 0 && (
+                        <Stack gap={1} direction="horizontal" className="flex-wrap">
+                            {note.tags.map(tag => (
+                                <Badge className="text-truncate" key={tag.id}>
+                                    {tag.label}
+                                </Badge>
+                            ))}
+                        </Stack>
+                    )}
+                </Col>
+                <Col xs="auto">
+                    <Stack gap={2} direction="horizontal">
+                        <Link to={`/${note.id}/edit`}>
+                            <Button variant="primary">Edit</Button>
+                        </Link>
+                        <Button onClick={() => {
+                            onDelete(note.id)
+                            navigate("/")
+                        }} variant="outline-danger">
+                            Delete
+                        </Button>
+                        <Link to="/">
+                            <Button variant="outline-secondary">Back</Button>
+                        </Link>
+                    </Stack>
+                </Col>
+            </Row>
+            <ReactMarkdown>{note.markdown}</ReactMarkdown>
+        </>
+    )
+}
